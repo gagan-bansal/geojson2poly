c0a96bb888563ec7d35ddc9b8f5db63ae0edb3f1
Gagan Bansal <gaganbansal123@gmail.com>
2016-08-29 07:39:53 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# geojson2poly
+Converts GeoJOSN polygons to osm poly format file.
+## installation
+
+```
+npm install geojson2poly -g
+```
+
+## usage
+```
+geojson2poly <geojson-file> <output-poly-file>
+```
+## license
+This project is licensed under the terms of the MIT license.
---
+++ geojson2poly.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+var multi = require('multigeojson')
+var extract = require('geojson-extract-geometries')
+var fs = require('fs')
+var path = require('path')
+var argv = require('yargs')
+  .usage('Usage: $0 <geojson-file> <output-poly-file>')
+  .demand(2)
+  .help('h')
+  .alias('h', 'help')
+  .argv
+fs.readFile(path.resolve(argv._[0]), function(err, data) {
+  if (err) throw err
+  var geojson = JSON.parse(data)
+  var polies = extract(geojson,'Polygon')
+  var name = path.basename(argv._[1]).split('.')[0]
+  var file = fs.createWriteStream(argv._[1])
+  file.on('error', function(err) { throw err})
+  polies.forEach(function(poly,ind) {
+    file.write(name + '-'+ ind + '\n' + 1 + '\n')
+    poly.coordinates[0].forEach(function(p) {
+      file.write('\t' + p.join('\t') + '\n')
+    })
+    file.write('END\nEND\n')
+  })
+  file.end()
+  console.log('Done!')
+})
